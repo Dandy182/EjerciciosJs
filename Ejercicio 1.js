692d873f5abc1aa71ff5b0a1ee6ae34d686b0f2a
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2022-04-13 21:52:03 -0500

    ejercicio 7

--- Ejercicio 1.js
+++ Ejercicio 1.js
@@ -201,7 +201,3 @@ function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
 
 priceWithMonthlyDiscount(89, 230, 0.42);
 // => 97972
-priceWithMonthlyDiscount(16, 70, 0);
-//=>8960
-priceWithMonthlyDiscount(16, 130, 0.15);
-//=>14528
